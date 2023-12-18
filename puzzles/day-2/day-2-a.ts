242d5d065cade92d666955764c30a781d2563b60
Gomius <j.prev@free.fr>
2023-12-18 22:17:00 +0100

    day-2-a

--- puzzles/day-2/day-2-a.ts
+++ puzzles/day-2/day-2-a.ts
@@ -1,9 +1,67 @@
 import { readData } from '../../shared.ts';
 import chalk from 'chalk';
 
+type game = {
+  id: number,
+  blue: number,
+  red: number,
+  green: number,
+  valid: boolean,
+}
+
 export async function day2a(dataPath?: string) {
   const data = await readData(dataPath);
-  return 0;
+  const bag = {
+    red: 12,
+    green: 13,
+    blue: 14,
+  }
+
+  console.time();
+
+  const games: Array<game> = [];
+  data.forEach(line => {
+    const dataLine = line.split(':');
+    const gameID = parseInt(dataLine[0].match(/(Game )(\d+)/)[2]);
+    const sets = dataLine[1].split(';');
+    const ga = <game>{
+      id: gameID,
+      red: 0,
+      green: 0,
+      blue: 0,
+      valid: true,
+    };
+
+    sets.forEach(set => {
+      const colors = set.split(',').map(color => color.trim());
+      colors.forEach(color => {
+        const colorCount = color.match(/(\d+) (red|green|blue)/);
+        switch (colorCount[2]) {
+          case 'red':
+            ga.red = parseInt(colorCount[1]);
+            if (ga.red > bag.red) ga.valid = false;
+            break;
+          case 'green':
+            ga.green = parseInt(colorCount[1]);
+            if (ga.green > bag.green) ga.valid = false;
+            break;
+          case 'blue':
+            ga.blue = parseInt(colorCount[1]);
+            if (ga.blue > bag.blue) ga.valid = false;
+        }
+      });
+    });
+
+    if (ga.valid) {
+      games.push(ga);
+    }
+
+  });
+
+  const result = games.reduce((a, b) => a + b.id, 0);
+
+  console.timeEnd();
+  return result;
 }
 
 const answer = await day2a();
